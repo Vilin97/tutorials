548da9d5422840cc82211fa87d5521b463cb20e3
Vasily Ilin <vasilin97@gmail.com>
2021-09-13 16:06:27 -0700

    finished tutorial 6

--- src/my_exercises/06_sub_sequences.lean
+++ src/my_exercises/06_sub_sequences.lean
@@ -52,7 +52,13 @@ inputs. -/
 -- 0039
 lemma extraction_ge : extraction φ → ∀ N N', ∃ n ≥ N', φ n ≥ N :=
 begin
-  sorry
+  intros h M N,
+  let n := M + N,
+  use n,
+  split,
+  linarith,
+  calc M ≤ n : by linarith
+    ...  ≤ φ n : id_le_extraction' h n,
 end
 
 /-- A real number `a` is a cluster point of a sequence `u` 
@@ -79,7 +85,11 @@ One gets used to it. Alternatively, one can get rid of it using the lemma
 lemma near_cluster :
   cluster_point u a → ∀ ε > 0, ∀ N, ∃ n ≥ N, |u n - a| ≤ ε :=
 begin
-  sorry
+  intros h ε ε_pos M,
+  rcases h with ⟨φ, φ_ext, hl ⟩,
+  cases (hl ε ε_pos) with N hN,
+  rcases (extraction_ge φ_ext M N) with ⟨ n, hn, h_φn⟩,
+  exact ⟨φ n, h_φn, hN n hn⟩,
 end
 
 /-
@@ -93,14 +103,20 @@ existential statements.
 lemma subseq_tendsto_of_tendsto' (h : seq_limit u l) (hφ : extraction φ) :
 seq_limit (u ∘ φ) l :=
 begin
-  sorry
+  intros ε ε_pos,
+  cases h ε ε_pos with N hN,
+  use N,
+  intros n hn,
+  exact hN (φ n) (by linarith [id_le_extraction' hφ n]),
 end
 
 /-- If `u` tends to `l` all its cluster points are equal to `l`. -/
 -- 0042
 lemma cluster_limit (hl : seq_limit u l) (ha : cluster_point u a) : a = l :=
 begin
-  sorry
+  rcases ha with ⟨ φ, φ_ext, h⟩,
+  apply unique_limit h,
+  exact subseq_tendsto_of_tendsto' hl φ_ext,
 end
 
 /-- Cauchy_sequence sequence -/
@@ -109,7 +125,14 @@ def cauchy_sequence (u : ℕ → ℝ) := ∀ ε > 0, ∃ N, ∀ p q, p ≥ N →
 -- 0043
 example : (∃ l, seq_limit u l) → cauchy_sequence u :=
 begin
-  sorry
+  rintros ⟨l, hl⟩ ε ε_pos,
+  cases hl (ε/2) (by linarith) with N hN,
+  use N,
+  intros p q hp hq,
+  calc |u p - u q| = |(u p - l) + (l - u q)| : by ring_nf
+               ... ≤ |(u p - l)| + |(l - u q)| : abs_add (u p - l) (l - u q)
+               ... = |u p - l| + |u q - l| : by rw abs_sub_comm l (u q)
+               ... ≤ ε : by linarith [hN p hp, hN q hq],
 end
 
 
@@ -120,6 +143,18 @@ In the next exercise, you can reuse
 -- 0044
 example (hu : cauchy_sequence u) (hl : cluster_point u l) : seq_limit u l :=
 begin
-  sorry
+  intros ε ε_pos,
+  cases hu (ε/2) (by linarith) with N hN,
+  rcases hl with ⟨ φ, φ_ext, φ_hl ⟩,
+  cases φ_hl (ε/2) (by linarith) with N' hN',
+  use N + N',
+  intros n hn,
+  have key1 : φ n ≥ N,
+    linarith [id_le_extraction φ_ext n],
+  have key2 : |(u n - (u ∘ φ) n)| ≤ ε/2,
+    exact hN n (φ n) (by linarith) key1,
+  calc |u n - l| = |(u n - (u ∘ φ) n) + ((u ∘ φ) n - l)| : by ring_nf
+             ... ≤ |(u n - (u ∘ φ) n)| + |((u ∘ φ) n - l)| : abs_add (u n - (u ∘ φ) n) ((u ∘ φ) n - l)
+             ... ≤ ε : by linarith [hN' n (by linarith)],
 end
 
